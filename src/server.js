698c28515dc46ed2282836bdde2022b5479ee0a0
Fernanda Dias <fernandadias9@hotmail.com>
2024-03-05 16:00:27 -0300

    initial commit, initialization in NodeJS, import and initialization of the express framework in the server.js file

---
+++ src/server.js
@@ -0,0 +1,9 @@
+/* Importing Express, the framework that handles HTTP requests. */
+const express = require("express");
+
+// Initializing Express
+const app = express();
+
+/*Indicates which port the express should listen to to respond to requests. Structure: the express initializer "listens" to the PORT and the arrow function indicates what it should do when this happens.*/
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
